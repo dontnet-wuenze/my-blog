3761012d7f8a6f25cb62165b1da50733674801bc
dotnet <alex0132@163.com>
2020-02-22 15:40:30 +0800

    table user

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,18 +1,32 @@
 import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
+export type gender = 'm'| 'f' | 'x';
 
 @Entity()
 export class User {
 
     @PrimaryGeneratedColumn()
-    id: number;
+    name: string;
 
     @Column()
-    firstName: string;
+    password: string;
 
     @Column()
-    lastName: string;
+    avatar: string;
+
+    @Column({
+        type : "enum",
+        enum : ['m', 'f', 'x'],
+        default : 'x'
+    })
+    gender : gender;
 
     @Column()
-    age: number;
-
+    bio : string
 }
+
+
+/*name: { type: 'string', required: true },
+  password: { type: 'string', required: true },
+  avatar: { type: 'string', required: true },
+  gender: { type: 'string', enum: ['m', 'f', 'x'], default: 'x' },
+  bio: { type: 'string', required: true }*/
