0b43cf0bd5bc0a292126d4765270728f1cf4506d
dotnet <alex0132@163.com>
2020-02-27 09:59:53 +0800

    modify

--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -6,37 +6,37 @@ const checkLogin = require('../middlewares/check').checkLogin
 
 // GET /posts 所有用户或者特定用户的文章页
 //   eg: GET /posts?author=xxx
-router.get('/',  (ctx) =>{
-ctx.body = ('主页')
+router.get('/',  async (ctx, next) =>{
+ await ctx.render('post');
 })
 
 // POST /posts/create 发表一篇文章
-router.post('/create', checkLogin,  (ctx) =>{
+router.post('/create', checkLogin, async (ctx, next) =>{
 ctx.body = ('发表文章')
 })
 
 // GET /posts/create 发表文章页
-router.get('/create', checkLogin,  (ctx) =>{
+router.get('/create', checkLogin, async (ctx, next) =>{
 ctx.body = ('发表文章页')
 })
 
 // GET /posts/:postId 单独一篇的文章页
-router.get('/:postId',  (ctx) =>{
+router.get('/:postId', async (ctx, next) =>{
 ctx.body = ('文章详情页')
 })
 
 // GET /posts/:postId/edit 更新文章页
-router.get('/:postId/edit', checkLogin,  (ctx) =>{
+router.get('/:postId/edit', checkLogin, async (ctx, next) =>{
 ctx.body = ('更新文章页')
 })
 
 // POST /posts/:postId/edit 更新一篇文章
-router.post('/:postId/edit', checkLogin,  (ctx) =>{
+router.post('/:postId/edit', checkLogin, async (ctx, next) =>{
 ctx.body = ('更新文章')
 })
 
 // GET /posts/:postId/remove 删除一篇文章
-router.get('/:postId/remove', checkLogin,  (ctx) =>{
+router.get('/:postId/remove', checkLogin, async (ctx, next) =>{
 ctx.body = ('删除文章')
 })
 
