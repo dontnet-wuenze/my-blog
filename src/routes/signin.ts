0b03f26b098a7c051706e336c0a7c3a564307a57
dotnet <alex0132@163.com>
2020-02-27 10:41:24 +0800

    add

--- src/routes/signin.ts
+++ src/routes/signin.ts
@@ -5,12 +5,12 @@ const router = new Router();
 const checkNotLogin = require('../middlewares/check').checkNotLogin
 
 // GET /signin 登录页
-router.get('/', checkNotLogin, (ctx) =>{
-ctx.body = ('登录页')
+router.get('/', checkNotLogin, async (ctx, next) =>{
+    await ctx.render('signin') 
 })
 
 // POST /signin 用户登录
-router.post('/', checkNotLogin, (ctx) =>{
+router.post('/', checkNotLogin, async (ctx, next) =>{
 ctx.body = ('登录')
 })
 
--- src/routes/signout.ts
+++ src/routes/signout.ts
@@ -5,8 +5,12 @@ const router = new Router()
 const checkLogin = require('../middlewares/check').checkLogin
 
 // GET /signout 登出
-router.get('/', checkLogin, (ctx) =>{
-  ctx.body = ('登出')
+router.get('/', checkLogin, async (ctx, next) =>{
+  // 清空 session 中用户信息
+  ctx.session.user = null
+  ctx.flash('success', '登出成功')
+  // 登出成功后跳转到主页
+  ctx.redirect('/posts')
 })
 
 module.exports = router.routes()
