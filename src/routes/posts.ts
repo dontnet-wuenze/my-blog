335b62702ec856ae1c534d6f1ab5a6d9710b049e
dotnet <alex0132@163.com>
2020-02-29 00:18:48 +0800

    add post article and check article

--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -1,4 +1,6 @@
 import "reflect-metadata";
+import { Post } from "../entity/Post";
+import { getRepository } from "typeorm";
 const Router = require('koa-router');
 const router = new Router();
 
@@ -6,13 +8,72 @@ const checkLogin = require('../middlewares/check').checkLogin
 
 // GET /posts 所有用户或者特定用户的文章页
 //   eg: GET /posts?author=xxx
-router.get('/',  async (ctx, next) =>{
-    await ctx.render('post');
+router.get('/',  async (ctx, next) => {
+    const author = ctx.query.author
+    try {
+        let postRepository = getRepository(Post);
+        let posts = await postRepository.find(author);
+        console.log(posts);
+        await ctx.render('post', {
+            posts : posts
+        })
+    } catch(err) {
+        throw err;
+        await next(err);
+    }
+  /*PostModel.getPosts(author)
+    .then(function (posts) {
+      res.render('posts', {
+        posts: posts
+      })
+    })
+    .catch(next)*/
 })
 
 // POST /posts/create 发表一篇文章
 router.post('/create', checkLogin, async (ctx, next) =>{
-    await ctx.render('create');
+    const author = ctx.session.user._id;
+    const title = ctx.request.body.title;
+    const content = ctx.request.body.content;
+  
+    // 校验参数
+    try {
+      if (!title.length) {
+        throw new Error('请填写标题')
+      }
+      if (!content.length) {
+        throw new Error('请填写内容')
+      }
+    } catch (err) {
+      ctx.flash('error', err.message)
+      return ctx.redirect('back')
+    }
+  
+    let post = {
+      author: author,
+      title: title,
+      content: content
+    }
+
+    try {
+        let postRepository = getRepository(Post);
+        let article = await postRepository.save(post);
+        ctx.flash('success', '发表成功');
+        ctx.redirect(`${article._id}`);
+    } catch(err) {
+        console.log(err);
+        await next(err);
+    }
+  
+    /*PostModel.create(post)
+      .then(function (result) {
+        // 此 post 是插入 mongodb 后的值，包含 _id
+        post = result.ops[0]
+        req.flash('success', '发表成功')
+        // 发表成功后跳转到该文章页
+        res.redirect(`/posts/${post._id}`)
+      })
+      .catch(next)*/
 })
 
 // GET /posts/create 发表文章页
@@ -22,7 +83,46 @@ router.get('/create', checkLogin, async (ctx, next) =>{
 
 // GET /posts/:postId 单独一篇的文章页
 router.get('/:postId', async (ctx, next) =>{
-ctx.body = ('文章详情页')
+    const postId = ctx.params.postId
+
+    let postRepository = getRepository(Post);
+
+    Promise.all([
+        postRepository.findOne(postId),
+        postRepository
+        .createQueryBuilder()
+        .update("post")
+        .set({pv: () => "'pv' + 1"
+        })
+        .execute()
+
+    ]) 
+      .then(async result => {
+        const post = result[0];
+        if(!post) {
+            throw new Error('该文章不存在');
+        }
+
+        await ctx.render('post', {
+            post : post
+        })
+      })
+      .catch(await next())
+   /* Promise.all([
+      PostModel.getPostById(postId), // 获取文章信息
+      PostModel.incPv(postId)// pv 加 1
+    ])
+      .then(function (result) {
+        const post = result[0]
+        if (!post) {
+          throw new Error('该文章不存在')
+        }
+  
+        res.render('post', {
+          post: post
+        })
+      })
+      .catch(next)*/
 })
 
 // GET /posts/:postId/edit 更新文章页
