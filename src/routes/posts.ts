b017f063da8e9f2dac68c250135b503efa8db2c7
dotnet <alex0132@163.com>
2020-02-29 16:52:07 +0800

    delete

--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -22,13 +22,6 @@ router.get('/',  async (ctx, next) => {
         console.log(err);
         await next();
     }
-  /*PostModel.getPosts(author)
-    .then(function (posts) {
-      res.render('posts', {
-        posts: posts
-      })
-    })
-    .catch(next)*/
 })
 
 // POST /posts/create 发表一篇文章
@@ -66,15 +59,6 @@ router.post('/create', checkLogin, async (ctx, next) =>{
         await next(err);
     }
   
-    /*PostModel.create(post)
-      .then(function (result) {
-        // 此 post 是插入 mongodb 后的值，包含 _id
-        post = result.ops[0]
-        req.flash('success', '发表成功')
-        // 发表成功后跳转到该文章页
-        res.redirect(`/posts/${post._id}`)
-      })
-      .catch(next)*/
 })
 
 // GET /posts/create 发表文章页
@@ -102,25 +86,6 @@ router.get('/:postId', async (ctx, next) =>{
     } catch(err) {
       console.log(err);
     }
-    /*Promise.all([
-        await postRepository.findOne({"_id" : postId}),
-        await postManager.increment(Post, {_id : postId}, "pv", 1)
-    ]) 
-      .then(result => {
-        const post = result[0];
-        console.log(result);
-        console.log(post);
-        if(!post) {
-            throw new Error('该文章不存在');
-        }
-        //ctx.body = post;
-        ctx.render('post', {
-            post : post
-        })
-      })
-      .catch(err => {
-        console.log(err);
-      })*/
 })
 
 // GET /posts/:postId/edit 更新文章页
@@ -144,19 +109,6 @@ router.get('/:postId/edit', checkLogin, async (ctx, next) =>{
   } catch(err) {
         console.log(err);
   }
-  /*PostModel.getRawPostById(postId)
-    .then(function (post) {
-      if (!post) {
-        throw new Error('该文章不存在')
-      }
-      if (author.toString() !== post.author._id.toString()) {
-        throw new Error('权限不足')
-      }
-      res.render('edit', {
-        post: post
-      })
-    })
-    .catch(next)*/
 })
 
 // POST /posts/:postId/edit 更新一篇文章
@@ -197,27 +149,33 @@ router.post('/:postId/edit', checkLogin, async (ctx, next) =>{
     console.log(err);
     await next();
   }
-  /*PostModel.getRawPostById(postId)
-    .then(function (post) {
-      if (!post) {
-        throw new Error('文章不存在')
-      }
-      if (post.author._id.toString() !== author.toString()) {
-        throw new Error('没有权限')
-      }
-      PostModel.updatePostById(postId, { title: title, content: content })
-        .then(function () {
-          req.flash('success', '编辑文章成功')
-          // 编辑成功后跳转到上一页
-          res.redirect(`/posts/${postId}`)
-        })
-        .catch(next)
-    })*/
 })
 
 // GET /posts/:postId/remove 删除一篇文章
 router.get('/:postId/remove', checkLogin, async (ctx, next) =>{
-ctx.body = ('删除文章')
+  const postId = ctx.params.postId
+  const author = ctx.session.user._id
+
+  const postRepository = getRepository(Post);
+
+  try {
+      let post = await postRepository.findOne({"_id" : postId});
+      if(!post) {
+        throw new Error('文章不存在')
+      }
+      if(post.author.toString() !== author.toString()) {
+        throw new Error('没有权限');
+      }
+      
+      await postRepository.delete(postId);
+      ctx.flash('success', '删除文章成功');
+      // 删除成功跳转主页
+      ctx.redirect('/posts');
+  } catch(err) {
+      console.log(err);
+      await next();
+  } 
+
 })
 
 module.exports = router.routes()
